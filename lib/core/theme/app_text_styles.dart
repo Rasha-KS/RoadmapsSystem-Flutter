68316580285bf0773293de6c3eba6edd46089edb
amera-altohami <ameraaltohami@gmail.com>
2026-02-01 21:07:00 +0200

    theme fonts and colors

---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle heading1 = TextStyle(
+    fontFamily: 'Tajawal_B',
+    fontSize: 42,
+    fontWeight: FontWeight.bold
+  );
+   static const TextStyle heading2 = TextStyle(
+    fontFamily: 'Tajawal_B',
+    fontSize: 35,
+    fontWeight: FontWeight.bold
+  );
+   static const TextStyle heading2_2 = TextStyle(
+    fontFamily: 'Tajawal_B',
+    fontSize: 29,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle heading3 = TextStyle(
+    fontFamily: 'Tajawal_R',
+    fontSize: 29,
+  );
+  static const TextStyle heading4 = TextStyle(
+    fontFamily: 'Tajawal_B',
+    fontSize: 24,
+    fontWeight: FontWeight.bold
+  );
+  static const TextStyle heading5 = TextStyle(
+    fontFamily: 'Tajawal_M',
+    fontSize: 20,
+  );
+  static const TextStyle boldHeading5 = TextStyle(
+    fontFamily: 'Tajawal_XB',
+    fontSize: 20,
+  );
+   static const TextStyle body = TextStyle(
+    fontFamily: 'Tajawal_R',
+    fontSize: 17,
+  );
+  static const TextStyle smallText = TextStyle(
+    fontFamily: 'Tajawal_R',
+    fontSize: 14,
+  );
+  static const TextStyle boldSmallText = TextStyle(
+    fontFamily: 'Tajawal_M',
+    fontSize: 14,
+  );
+}
